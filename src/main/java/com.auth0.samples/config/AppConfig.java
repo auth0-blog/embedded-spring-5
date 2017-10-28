1ef31b445d589fc9ac8a9abebd71f48d419ee2ff
Bruno Krebs <bruno.krebs@auth0.com>
2017-10-28 20:11:01 -0200

    Reorganizing packages and cleaning dependencies.

---
+++ src/main/java/com.auth0.samples/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.auth0.samples.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.auth0.samples"})
+public class AppConfig {
+
+}
