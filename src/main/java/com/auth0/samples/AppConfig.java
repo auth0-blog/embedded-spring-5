bdadac8100c50a2d5d22d18cf2465165fcfa5a8f
Bruno Krebs <bruno.krebs@auth0.com>
2017-10-30 10:13:44 -0200

    Consolidate WebInit and AppConfig in a single file; Ignoring .out directory created by IntelliJ

--- src/main/java/com/auth0/samples/AppConfig.java
+++ src/main/java/com/auth0/samples/AppConfig.java
@@ -1,14 +1,20 @@
-package com.auth0.samples.config;
+package com.auth0.samples;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
 
-public class WebAppInitializer implements WebApplicationInitializer {
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.auth0.samples"})
+public class AppConfig implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext container) {
